abc4cc0f6af90763251519cadb4738d88bb8ab22
Ed Kalosha <eduard.kalosha@billingplatform.com>
2021-06-18 00:15:11 +0300

    refactor 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,26 @@ import logo from './logo.svg';
 import './App.css';
  
 import {Header} from './Header'
-import {AppItem} from './AppItem'
+import {AppItemList} from './AppItemList'
 import {AddItemsArea} from './AddItemsArea'
 import {ItemsArea} from './ItemsArea'
-import {HashString,ItemType} from './types'
+import {AppItemsList} from './AppItemList/types'
+import {getNewItems} from './tools'
  
  
-const App = ():JSX.Element =>{
-    const defaultHashLength = 10;
-    const defaultCountItems = 20; 
-    // items - List of items that can be updated with new additional items when the button is pressed
-    const [items, setItems] = React.useState<ItemType[]>([]); 
-    React.useEffect(()=>{
-        fillItems(); // Fills the List onmount
-    },[])    
-    const generateHash = (hashLength = defaultHashLength):HashString=>{
-        var result = '';
-        const hashSymbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const hashSymbolsLen = hashSymbols.length;
-        for (let i = 0; i < hashLength; i++) {
-            result += hashSymbols.charAt(Math.floor(Math.random() * hashSymbolsLen));
-        }
-        return result;
-    }
-    const fillItems = ():void=>{ 
-        const newItems = [...Array(defaultCountItems)].map((_,id)=>({
-            id,title:generateHash()
-        }));
+const App:React.FC = () =>{ 
+    const [items, setItems] = React.useState<AppItemsList>(getNewItems);  
+    const addItems = ():void=>{ 
+        const newItems = getNewItems();
         setItems(items.length>0? [...items,...newItems]:newItems);
-    }  
-    const appItems = items.map(el=><AppItem title={el.title}/>);
-    
+    } 
     return(
         <div className="App">
             <Header logo={logo} alt={'App logo'}/>
-            <AddItemsArea onAdd={fillItems}/>
-            <ItemsArea> {appItems} </ItemsArea>
+            <AddItemsArea onAdd={addItems}/>
+            <ItemsArea> 
+                <AppItemList items={items} /> 
+            </ItemsArea>
         </div>
     )
 }
